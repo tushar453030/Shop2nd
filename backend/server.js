7687e211df2b0e6d8bd889d3277df53bf2dd08c7
tushar453030 <tushar4530@gmail.com>
2023-01-24 17:22:30 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import productRoutes from './routes/productRoutes.js'
 import products from './data/products.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+const path= require("path");
 dotenv.config()
 
 connectDB()
@@ -27,5 +28,10 @@ app.use('/api/orders',orderRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
+
+app.use(express.static(path.join(__dirname,'./frontend/build')));
+app.get('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./frontend/build/index.html'));
+});
 const PORT= process.env.PORT || 5000
 app.listen(PORT,console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
